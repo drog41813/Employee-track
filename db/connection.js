83803c9c2c65c4ea54ed552c6c1bcede95b117bb
drog41813 <daniel.lee042003@gmail.com>
2023-08-03 23:18:09 -0500

    added SQL password

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connect = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'MySQLPassword041803!',
   database: 'company_db'
 })
 module.exports = connect;
